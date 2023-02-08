a44a528e37ab4a5b0b59d0e23386545340e994f8
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-08 00:19:24 -0800

    add about component

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>Amaryah Wolf</h1>
+      <p>
+        {/* TODO: add About Me content */}
+      </p>
+    </div>
+  );
+}

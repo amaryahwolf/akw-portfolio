a3e186f31f367bd4c6387597dedd2051c85affe1
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-07 23:39:41 -0800

    add portfolio container component

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+// Import react, useState and components
+import React, { useState } from 'react';
+import Navigation from './Navigation';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+
+export default function PortfolioContainer() {
+    // Create useState with currentPage variable and setCurrentPage function
+    const [currentPage, setCurrentPage] = useState('About');
+
+    // Use conditional logic to render components
+    const renderPage = () => {
+        if (currentPage === 'About') {
+          return <About />;
+        }
+        if (currentPage === 'Projects') {
+          return <Projects />;
+        }
+        if (currentPage === 'Resume') {
+          return <Resume />;
+        }
+        return <Contact />;
+      };
+
+    // Create function to handle page change
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    // Return a component to Navigation with useState variable and function, and call renderPage function
+    <div>
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />  
+      {renderPage()}
+    </div>
+
+}

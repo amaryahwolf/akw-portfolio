c2431da5b64f7c00a78c2a6156fa810b473ebfdf
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-08 20:28:00 -0800

    add starter styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,16 @@ import React from 'react';
 
 function Footer() {
     return (
-        <ul className="">
-            <li className="">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <ul className="d-flex nav col-md-4 justify-content-end list-unstyled">
+            <li className="ms-3">
                 <a
                     href="https://www.linkedin.com/in/amaryah-wolf" className="">
                     {/* TODO: add linkedin icon */}
                     linkedin    
                 </a>
             </li>
-            <li className="">
+            <li className="ms-3">
                 <a
                     href="https://github.com/amaryahwolf" className="">
                     {/* TODO: add github icon */}
@@ -20,6 +21,7 @@ function Footer() {
             </li>
             {/* TODO: add third platform */}
         </ul>
+        </footer>
     )
 };
 

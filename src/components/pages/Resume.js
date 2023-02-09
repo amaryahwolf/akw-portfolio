6eb787b476cdc72d0e1270be35dd2bd0a3131eb6
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-08 20:18:34 -0800

    update resume component with proficiencies and downloadable resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,58 @@ import React from 'react';
 
 export default function Resume() {
     return (
-    <h1>Resume Content</h1>
+      <div>
+    <h1>Resume</h1>
+    <h2>Technical Skills and Proficiencies</h2>
+    {/* TODO: add blurb about favorite part of coding */}
+    <ul>
+      <li>
+        HTML, CSS
+      </li>
+      <li>
+        JavaScript
+      </li>
+      <li>
+        jQuery, Bootstrap
+      </li>
+      <li>
+        Web, Serverside and Third-Party APIs
+      </li>
+      <li>
+        Node.js, Express.js, Jest
+      </li>
+      <li>
+        Object-Oriented Programming
+      </li>
+      <li>
+        SQL, MySQL, Sequelize
+      </li>
+      <li>
+        Handlebars
+      </li>
+      <li>
+        Model-View-Controller Framework
+      </li>
+      <li>
+        Object-Relational Mapping
+      </li>
+      <li>
+        NoSQL, MongoDB, Mongoose
+      </li>
+      <li>
+        React
+      </li>
+      <li>
+        Progressive Web Apps
+      </li>
+      <li>
+        Agile Methodology
+      </li>
+      <li>
+        Git, Heroku
+      </li>
+    </ul>
+    <a href="https://docs.google.com/document/d/1-A6arbukBKoKJ_yGf6pPbeJkxxXGFr2BObC0_WwnNiE/export?format=pdf">Download Resume</a>
+    </div>
     );
   }
